68636af12d87842ae01dad9129f7744e828378cd
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-27 18:54:37 -0400

    pre-deploy config

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 root.render(
   <React.StrictMode>
     <Provider store={store}>
